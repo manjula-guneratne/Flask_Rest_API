1b631e1e816979eba9e405a698077e845dc8e0de
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2024-02-04 22:22:49 -0500

    test file added

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
+
+
+api.add_resource(HelloWorld, "/helloworld")
+
 if __name__ == "__main__":
     app.run(debug=True)
