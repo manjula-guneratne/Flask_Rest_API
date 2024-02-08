81f9cab79236edbe932cce888ae656e6da8c21f8
Manjula Guneratne <uftcodingbootcamp@gmail.com>
2024-02-08 06:44:38 -0500

    Complete

--- main.py
+++ main.py
@@ -53,20 +53,33 @@ class Video(Resource):
         db.session.commit()
         return video, 201
     
+    @marshal_with(resource_fields)
     def patch(self, video_id):
-        args = video_put_args.parse_args()
+        args = video_update_args.parse_args()
         result = VideoModel.query.filter_by(id=video_id).first()
-        if result:
-            abort(404, message="Video id taken...")
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
+
+        if args['name']:
+            result.name = args['name']
+        if args['views']:
+            result.views = args['views']
+        if args['likes']:
+            result.likes = args['likes']
+
+        db.session.commit()
+
+        return result
+
     
     def delete(self, video_id):
         # abort_if_video_id_doesnt_exist(video_id)
         # del videos[video_id]
         # return '', 204
+        pass
 
 
 api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
-  
--- test.py
+++ test.py
@@ -2,14 +2,18 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-data = [{"likes": 78, "name": "Joe", "views": 100000}, 
-        {"likes": 1000, "name": "Roger", "views": 200000},
-        {"likes": 35, "name": "Tim", "views": 80}]
+# Just to Test Patch
+# data = [{"likes": 78, "name": "Joe", "views": 100000}, 
+#         {"likes": 1000, "name": "Roger", "views": 200000},
+#         {"likes": 35, "name": "Tim", "views": 80}]
 
-for i in range(len(data)):
-    response = requests.put(BASE + "video/" + str(i), data[i])
-    print(response.json())
+# for i in range(len(data)):
+#     response = requests.put(BASE + "video/" + str(i), data[i])
+#     print(response.json())
 
-input()
-response = requests.get(BASE + "video/2")
+# input()
+# response = requests.get(BASE + "video/2")
+# print(response.json())
+
+response = requests.patch(BASE + "video/2", {"views":99, "likes":101})
 print(response.json())
